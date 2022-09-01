c7e72c9783b7f3e9d033663e2cc76628bf520152
Md Suhail Arif <suhailarif14@gmail.com>
2022-09-01 14:20:26 +0530

    login

--- src/Utils/StateProvider.jsx
+++ src/Utils/StateProvider.jsx
@@ -2,8 +2,10 @@ import { Children, createContext, useContext, useReducer } from "react";
 
 export const StateContext = createContext();
 
-export const StateProvider = ({children, initialState, reducer}) => {
+export const StateProvider = ({children, initialState, reducer}) => (
     <StateContext.Provider value={ useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
-}
+);
+
+export const useStateProvider = () => useContext(StateContext);
