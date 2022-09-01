08122289e97efabd2a8e62814a7b27bccae8c161
Md Suhail Arif <suhailarif14@gmail.com>
2022-09-01 16:43:17 +0530

    login token

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import styled from 'styled-components';
 
+
 export default function Login() {
+
+    const handleClick = () => {
+    const clientId = "5fc20efb01544c46bd202e4bfd1dacb3";
+    const redirectUrl = "http://localhost:3000/";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [ 'user-read-email',
+                    'user-read-private',
+                    'user-modify-playback-state',
+                    'user-read-playback-state', 
+                    'user-read-currently-playing', 
+                    'user-read-recently-played',
+                    'user-read-playback-position',
+                    'user-top-read'
+                ];
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}&response_type=token&show_daialog=true`;
+    };
+    
   return (
     <Container>
-        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="" />
-        <button>Connect Spotify</button>
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="spotify" />
+        <button onClick={handleClick}>Connect Spotify</button>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+height: 100vh;
+width: 100%;
+background-color: #1db954;
+gap: 5rem;
+img {
+    width: 66vw;
+}
+button{
+    width: 35vw;
+    padding: 2vh 5vw;
+    border-radius: 5rem;
+    border: none;
+    background-color: black;
+    color: #49f585;
+    font-size: 1.4rem;
+    cursor: pointer;
+}
+`;
+
